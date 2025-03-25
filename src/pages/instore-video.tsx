b99e1f124392dfd5ebbba21f6e8ed2ed8e5bb4a1
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2025-03-25 20:47:47 +0530

    fix

--- src/pages/instore-video.tsx
+++ src/pages/instore-video.tsx
@@ -100,109 +100,24 @@ const InstoreVideo = () => {
             className="absolute inset-0 bg-center bg-cover opacity-10"
             style={{ backgroundImage: "url('/bv1.png')" }}
           ></div>
-          <div className="relative z-10 text-left mt-10">
-            <p className="text-base sm:text-lg md:text-2xl leading-8 text-black mb-4 font-bold">
-              <ul className="m-5">
-                <li>• Video has been a proven and effective sales and marketing tool for over four decades, especially inside stores.</li>
-                <li>• Now, bespoke, store-branded downloadable videos are available to play in your store.</li>
-                <li>• The in-store video range is accessible to all registered stores and businesses.</li>
-                <li>• Any store wishing to sign up and register can do so by visiting the REGISTER page and filling in the required information.</li>
-                <li>• Simply upload a 2MB file of your store or business logo, and we will edit it into a 3-hour video that appears on screen every 5 minutes.</li>
-                <li>• We offer a wide variety of in-store videos, from single-category options to fully mixed product spreads.</li>
-              </ul>
-              <br />
-              <ul className="m-5">
-                <li>We have a huge choice of in-store videos to choose from, ranging from a single solus category to a fully mixed product spread.</li>
-                <li>Available categories include:</li><br />
-                <li><a href="/phones" className="text-blue-500 hover:underline">PHONES</a></li>
-                <li><a href="/pc" className="text-blue-500 hover:underline">PC</a></li>
-                <li><a href="/audio" className="text-blue-500 hover:underline">AUDIO</a></li>
-                <li><a href="/video-games" className="text-blue-500 hover:underline">VIDEO GAMES</a></li>
-                <li><a href="/tech" className="text-blue-500 hover:underline">TECH</a></li>
-                <li><a href="/merchandise" className="text-blue-500 hover:underline">MERCHANDISE</a></li>
-                <li><a href="/tcg" className="text-blue-500 hover:underline">TCG (TRADING CARD GAMES)</a></li>
-              </ul>
-            </p>
-          </div>
-        </div>
-
-        <div className="relative w-full mt-10">
-          <div
-            className="absolute inset-0 bg-center bg-cover opacity-10"
-            style={{ backgroundImage: "url('/bv2.png')" }}
-          ></div>
-          <div className="relative z-10 mt-10 p-5">
-            <h2 className="text-3xl font-bold mb-4">Do You Require More Than One Category?</h2>
+          <div className="relative z-10 text-center mt-10 m-2">
             <p className="text-base sm:text-lg md:text-xl leading-8 text-black mb-4 font-bold">
-              <ul className="list-disc pl-5 text-lg sm:text-xl">
-                <li><a className="text-blue-500 hover:underline">PHONES & PC & AUDIO & VIDEO GAMES & TECH & MERCHANDISE & TCG</a></li>
-                <li><a className="text-blue-500 hover:underline">PHONES & PC & AUDIO & VIDEO GAMES & TECH</a></li>
-                <li><a className="text-blue-500 hover:underline">PHONES & PC & AUDIO & VIDEO GAMES</a></li>
-                <li><a className="text-blue-500 hover:underline">PHONES & PC</a></li>
-                <li><a className="text-blue-500 hover:underline">PHONES & PC & VIDEO GAMES</a></li>
-                <li><a className="text-blue-500 hover:underline">PC & VIDEO GAMES</a></li>
-                <li><a className="text-blue-500 hover:underline">PC & AUDIO</a></li>
-                <li><a className="text-blue-500 hover:underline">PC & AUDIO & VIDEO GAMES</a></li>
-                <li><a className="text-blue-500 hover:underline">VIDEO GAMES & MERCHANDISE</a></li>
-                <li><a className="text-blue-500 hover:underline">VIDEO GAMES & MERCHANDISE & TCG</a></li>
-                <li><a className="text-blue-500 hover:underline">VIDEO GAMES & TCG</a></li>
-                <li><a className="text-blue-500 hover:underline">MERCHANDISE & TCG</a></li>
-              </ul>
+            You’re now just a few clicks away from having your own personalised in-store video.
             </p>
-            <p className="mt-6 text-lg sm:text-xl md:text-2xl">
-              For any other requirements or queries, please email us at <a href="mailto:POSVideo@instorenetwork.com" className="text-blue-500">POSVideo@instorenetwork.com</a>
+            <p className='text-base sm:text-lg md:text-xl leading-8 text-black mb-4 font-bold'>
+            Our videos will play on any device and are suitable for a family audience. There is no violent, or sexual content and no language which might cause offence. They are made to full high quality television broadcast standard
             </p>
+            <p className="text-base sm:text-lg md:text-xl leading-8 text-black mb-4 font-bold">
+            Here is a simple, step-by-step guide to obtaining your in-store video
+            </p>
+            <ul>
+              <p className='text-lg font-semibold'>1. CLICK AND SELECT YOUR VIDEO</p>
+            </ul>
           </div>
         </div>
-         {/* Instruction Section */}
-         <div className="w-full max-w-4xl p-8 mt-10 rounded-xl">
-          <h2 className="text-3xl font-bold mb-6 text-center">How to Use In-Store Video</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6">
-            {/* Step 1 */}
-            <div className="bg-white p-6 rounded-lg shadow-md border border-gray-200">
-              <h3 className="text-xl font-semibold mb-4">1. SELECT A VIDEO</h3>
-              <p className="text-base text-gray-700">
-                We have a huge range of videos to choose from that are perfect for in-store displays. Browse through our library and pick the one that suits your needs.
-              </p>
-                            {/* Add images for logo examples */}
-                            <div className="flex justify-center mt-4">
-                <img src="/logoDemo2.png" alt="Example 1" className="p-2 object-contain" />
-              </div>
-            </div>
-
-            {/* Step 2 */}
-            <div className="bg-white p-6 rounded-lg shadow-md border">
-              <h3 className="text-xl font-semibold mb-4">2. PREPARE AND UPLOAD YOUR LOGO</h3>
-              <p className="text-base text-gray-700">
-                We recommend using a PNG file with a transparent background for the best results.There are many free tools online (including Adobe) if you need help creating your logo. Here are some examples:
-              </p>
-              {/* Add images for logo examples */}
-              <div className="flex justify-center mt-4">
-                <img src="/logoDemo1.png" alt="Example 2" className="p-2 object-contain" />
-              </div>
-            </div>
-
-            {/* Step 3 */}
-            <div className="bg-white p-6 rounded-lg shadow-md border border-gray-200">
-              <h3 className="text-xl font-semibold mb-4">3. WAIT FOR THE VIDEO TO PROCESS</h3>
-              <p className="text-base text-gray-700">
-              The videos are made to a high standard and have a long running time. Please be patient and wait for the video to process. It's no different to waiting for YouTube videos to prcoess after they have been uploaded.
-              </p>
-            </div>
-
-            {/* Step 4 */}
-            <div className="bg-white p-6 rounded-lg shadow-md border border-gray-200">
-              <h3 className="text-xl font-semibold mb-4">4. VIDEO DOWNLOADS AUTOMATICALLY</h3>
-              <p className="text-base text-gray-700">
-              Once your video has finished processing it will automatically download to your device and will be ready for use.
-              </p>
-            </div>
-          </div>
-        </div>
-
         <div className="flex items-center justify-center px-4 mt-8 md:mt-8 sm:px-8 md:px-12">
           <div className="p-6 w-full max-w-4xl bg-white rounded-xl shadow-lg">
-            <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">Select video and upload logo</h2>
+            {/* <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">Select video and upload logo</h2> */}
             <form onSubmit={handleProcessVideo} className="space-y-8">
               <div>
                 <label className="block mb-2 text-lg font-semibold text-gray-700">Select video:</label>
@@ -236,15 +151,58 @@ const InstoreVideo = () => {
               </div>
 
               <div>
-                <label className="block mb-2 text-lg font-semibold text-gray-700">Upload logo:</label>
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handleLogoUpload}
-                  required
-                  className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gray-50"
-                />
-              </div>
+  <label className="block mb-2 text-lg font-semibold">2. PREPARE AND UPLOAD YOUR LOGO</label>
+  
+  <p className="text-base sm:text-lg md:text-xl leading-8 text-black mb-4 font-bold">
+    We recommend using a PNG file with a transparent background for the best results. 
+    There are many free tools online (including Adobe) if you need help creating your logo. 
+    <br /> Here are some examples:
+  </p>
+
+  <div className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-2 gap-4 mt-4">
+    {/* Logo Example 1 */}
+    <div className="flex flex-col items-center w-1/2">
+      <img src="/logoDemo1.png" alt="Logo Example 1" className="p-2 object-contain" />
+      <p className="text-center mt-2">WIDTH:1829 x HEIGHT:564</p>
+    </div>
+    
+    {/* Logo Example 2 */}
+    <div className="flex flex-col items-center">
+      <img src="/logoDemo2.png" alt="Logo Example 2" className="p-2 object-contain" />
+    </div>
+
+    {/* Logo Example 3 */}
+    <div className="flex flex-col items-center w-1/2">
+      <img src="/logoDemo3.png" alt="Logo Example 3" className="p-2 object-contain" />
+      <p className="text-center mt-2">WIDTH:1829 x HEIGHT:1250</p>
+    </div>
+
+    {/* Logo Example 4 */}
+    <div className="flex flex-col items-center">
+      <img src="/logoDemo4.png" alt="Logo Example 4" className="p-2 object-contain" />
+    </div>
+
+    {/* Logo Example 5 */}
+    <div className="flex flex-col items-center w-1/2">
+      <img src="/logoDemo5.png" alt="Logo Example 5" className="p-2 object-contain" />
+      <p className="text-center mt-2">WIDTH:2072 x HEIGHT:1260</p>
+    </div>
+
+    {/* Logo Example 6 */}
+    <div className="flex flex-col items-center">
+      <img src="/logoDemo6.png" alt="Logo Example 6" className="p-2 object-contain" />
+    </div>
+  </div>
+
+  <input
+    type="file"
+    accept="image/*"
+    onChange={handleLogoUpload}
+    required
+    className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gray-50 mt-4"
+  />
+</div>
+
 
               <div className="flex justify-center">
                 <button
@@ -260,6 +218,85 @@ const InstoreVideo = () => {
             {message && <p className="mt-6 text-center text-gray-600">{message}</p>}
           </div>
         </div>
+
+        <div className="relative w-full mt-10">
+          <div
+            className="absolute inset-0 bg-center bg-cover opacity-10"
+            style={{ backgroundImage: "url('/bv2.png')" }}
+          ></div>
+        </div>
+         {/* Instruction Section */}
+         <div className="w-full max-w-4xl p-8 mt-10 rounded-xl">
+          <h2 className="text-3xl font-bold mb-6 text-center">How to Use In-Store Video</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6">
+            {/* Step 1 */}
+            <div className="bg-white p-6 rounded-lg shadow-md border border-gray-200">
+              <h3 className="text-xl font-semibold mb-4">1. SELECT A VIDEO</h3>
+              <p className="text-base text-gray-700">
+                We have a huge range of videos to choose from that are perfect for in-store displays. Browse through our library and pick the one that suits your needs.
+              </p>
+                            {/* Add images for logo examples */}
+                            <div className="flex justify-center mt-4">
+                <img src="/logoDemo2.png" alt="Example 1" className="p-2 object-contain" />
+              </div>
+            </div>
+
+            {/* Step 2 */}
+            <div className="bg-white p-6 rounded-lg shadow-md border">
+              <h3 className="text-xl font-semibold mb-4">2. PREPARE AND UPLOAD YOUR LOGO</h3>
+              <p className="text-base text-gray-700">
+                We recommend using a PNG file with a transparent background for the best results. There are many free tools online (including Adobe) if you need help creating your logo. Here are some examples:
+              </p>
+
+              {/* Add images for logo examples */}
+              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-4 mt-4">
+                <div className="flex flex-col items-center">
+                  <img src="/logoDemo1.png" alt="Logo Example 1" className="p-2 object-contain" />
+                  <p className="text-center mt-2">1829 x 564</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                  <img src="/logoDemo3.png" alt="Logo Example 2" className="p-2 object-contain" />
+                  <p className="text-center mt-2">1829 x 1250</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                  <img src="/logoDemo4.png" alt="Logo Example 3" className="p-2 object-contain" />
+                  <p className="text-center mt-2">1829 x 1250</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                  <img src="/logoDemo5.png" alt="Logo Example 4" className="p-2 object-contain" />
+                  <p className="text-center mt-2">2072 x 1260</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                  <img src="/logoDemo6.png" alt="Logo Example 5" className="p-2 object-contain" />
+                  <p className="text-center mt-2">2072 x 1260</p>
+                </div>
+              </div>
+
+            </div>
+
+
+            {/* Step 3 */}
+            <div className="bg-white p-6 rounded-lg shadow-md border border-gray-200">
+              <h3 className="text-xl font-semibold mb-4">3. WAIT FOR THE VIDEO TO PROCESS</h3>
+              <p className="text-base text-gray-700">
+              The videos are made to a high standard and have a long running time. Please be patient and wait for the video to process. It's no different to waiting for YouTube videos to prcoess after they have been uploaded.
+              </p>
+            </div>
+
+            {/* Step 4 */}
+            <div className="bg-white p-6 rounded-lg shadow-md border border-gray-200">
+              <h3 className="text-xl font-semibold mb-4">4. VIDEO DOWNLOADS AUTOMATICALLY</h3>
+              <p className="text-base text-gray-700">
+              Once your video has finished processing it will automatically download to your device and will be ready for use.
+              </p>
+            </div>
+          </div>
+        </div>
+
 
         <AdsSection numOfAds={3} position="homepage" />
       </div>
